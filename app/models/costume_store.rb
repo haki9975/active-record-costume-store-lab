942e0fe2f82d568ac17feffd51efb68aa4cf7f26
haki9975 <elias.robert.hakim@gmail.com>
2021-03-15 20:24:48 -0400

    commit tree f73a3365c869c5df06e982b5bb6af1212a0933df

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,7 @@
 # Create your CostumeStore class here
+# Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Base
+  
+end
+  
+  # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
